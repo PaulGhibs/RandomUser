a9c15de8c8d47fb4aa4f247797e6463ff56f44cd
Paul Ghibs <paul.ghib@gmail.com>
2022-02-22 15:21:14 +0100

    Initial Commit

---
+++ RandomUser/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RandomUser
+//
+//  Created by Paul Ghibeaux on 22/02/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
